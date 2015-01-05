451936a95e641e3f065d6e32f4a64ffdd4b7fe6f
Magellnea <magellanea@gmail.com>
2015-01-05 23:49:51 +0200

    Adding c++ Ide configurations

--- init.el
+++ init.el
@@ -92,3 +92,29 @@
 (global-set-key (kbd "C-&") 'ecb-goto-window-sources)
 (global-set-key (kbd "C-*") 'ecb-goto-window-methods)
 (global-set-key (kbd "C-(") 'ecb-goto-window-compilation)
+
+
+;; Irony-mode for c++
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+
+;; replace the `completion-at-point' and `complete-symbol' bindings in
+;; irony-mode's buffers by irony-mode's function
+(defun my-irony-mode-hook ()
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
+(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+;; autocomplete and c++ general configurations
+
+(require 'cc-mode)
+(setq-default c-basic-offset 4 c-default-style "linux")
+(setq-default tab-width 4 indent-tabs-mode t)
+(define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
+
+(require 'auto-complete-clang)
+(define-key c++-mode-map (kbd "<C-tab>") 'ac-complete-clang)
