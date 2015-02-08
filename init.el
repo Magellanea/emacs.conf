30fb5cd791fb79d5f9f9cfa00ed2a2f50a578345
Magellnea <magellanea@gmail.com>
2015-02-08 13:55:11 +0200

    Adding Elpy keybindings

--- init.el
+++ init.el
@@ -88,6 +88,7 @@
 	lua-mode
 	cython-mode
 	lush-theme
+	irony
     )
   "A list of packages to install from MELPA at launch.")
 
@@ -163,9 +164,19 @@ Missing packages are installed automatically."
 (global-set-key (kbd "C-&") 'ecb-goto-window-sources)
 (global-set-key (kbd "C-*") 'ecb-goto-window-methods)
 (global-set-key (kbd "C-(") 'ecb-goto-window-compilation)
+;;; Elpy keybinsings
+(global-set-key (kbd "<M-S-return>") 'elpy-goto-definition)
+;;c++ mode
+(require 'cc-mode)
+(setq-default c-basic-offset 4 c-default-style "linux")
+(setq-default tab-width 4 indent-tabs-mode t)
+(define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
 
+;; company mode autocomplete
+(add-hook 'after-init-hook 'global-company-mode)
 
-;; Irony-mode for c++
+;;Add irony-company to company list
+
 (add-hook 'c++-mode-hook 'irony-mode)
 (add-hook 'c-mode-hook 'irony-mode)
 (add-hook 'objc-mode-hook 'irony-mode)
@@ -180,18 +191,7 @@ Missing packages are installed automatically."
 (add-hook 'irony-mode-hook 'my-irony-mode-hook)
 (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
 
-;; autocomplete and c++ general configurations
 
-(require 'cc-mode)
-(setq-default c-basic-offset 4 c-default-style "linux")
-(setq-default tab-width 4 indent-tabs-mode t)
-(define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
-
-(require 'auto-complete-clang)
-(define-key c++-mode-map (kbd "<C-tab>") 'ac-complete-clang)
-
-;; company mode autocomplete
-(add-hook 'after-init-hook 'global-company-mode)
 
 ;; AUCTex plugin
 (load "auctex.el" nil t t)
